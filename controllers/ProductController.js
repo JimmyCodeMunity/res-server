e5c7af1118db701d578292624c4dd375b94f1b21
JimmyCodeMunity <jameswafula2002@gmail.com>
2024-04-27 10:03:35 +0300

    i dunno

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -5,84 +5,84 @@ const asyncHandler = require('express-async-handler');
 
 //get all the products
 const getAllProducts = async (req, res) => {
-    try {
-        const product = await Product.find().sort({
-            createdAt: -1,
-        });
-        res.status(200).json(product);
+  try {
+    const product = await Product.find().sort({
+      createdAt: -1,
+    });
+    res.status(200).json(product);
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: error.message })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message })
 
-    }
+  }
 }
 
 //get all subcategories
 const getSubcategories = async (req, res) => {
-    try {
-        const brands = await Brand.find({}); // Fetch all brands
-        const subcategories = brands.reduce((acc, brand) => {
-            // Extract individual subcategories from each brand's subcategories array
-            brand.subcategories.forEach(subcategory => {
-                if (!acc.includes(subcategory)) {
-                    acc.push(subcategory);
-                }
-            });
-            return acc;
-        }, []);
+  try {
+    const brands = await Brand.find({}); // Fetch all brands
+    const subcategories = brands.reduce((acc, brand) => {
+      // Extract individual subcategories from each brand's subcategories array
+      brand.subcategories.forEach(subcategory => {
+        if (!acc.includes(subcategory)) {
+          acc.push(subcategory);
+        }
+      });
+      return acc;
+    }, []);
 
-        res.status(200).json(subcategories);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Subcategories not available' });
-    }
+    res.status(200).json(subcategories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Subcategories not available' });
+  }
 }
 
 //get product by category
 const getProductByCategory = async (req, res) => {
-    try {
-        const { category } = req.params;
+  try {
+    const { category } = req.params;
 
-        const product = await Product.find({ category });
-        if (!product) {
-            res.status(404).json({ message: 'you have no category' });
-        }
-        res.status(200).json(product);
+    const product = await Product.find({ category });
+    if (!product) {
+      res.status(404).json({ message: 'you have no category' });
     }
-    catch (error) {
-        res.status(500).json({ message: error.message });
+    res.status(200).json(product);
+  }
+  catch (error) {
+    res.status(500).json({ message: error.message });
 
-    }
+  }
 }
 
 //get product by shopid
-const getProductByShopId = async(req,res)=>{
-    try {
-      const { supplier } = req.params;
-  
-      // Assuming you have a Product model with a 'shopId' field
-      const products = await Product.find({ supplier });
-      console.log(supplier)
-  
-      res.status(200).json(products);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  }
+const getProductByShopId = async (req, res) => {
+  try {
+    const { supplier } = req.params;
 
-  const getProductsBySupplier = asyncHandler(async (req, res) => {
-    try {
-        const supplierId = req.params.supplier;
-        const products = await Product.find({ supplier: supplierId });
-        res.status(200).json({ success: true, products });
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Failed to fetch products' });
-    }
+    // Assuming you have a Product model with a 'shopId' field
+    const products = await Product.find({ supplier });
+    console.log(supplier)
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+const getProductsBySupplier = asyncHandler(async (req, res) => {
+  try {
+    const { supplier } = req.params;
+    const products = await Product.find({ supplier });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 })
 
-const searchProducts = async(req,res)=>{
-    try {
+const searchProducts = async (req, res) => {
+  try {
     const { query } = req.params;
 
     const products = await Product.find({
@@ -106,10 +106,10 @@ const searchProducts = async(req,res)=>{
 
 
 module.exports = {
-    getAllProducts,
-    getSubcategories,
-    getProductByCategory,
-    getProductByShopId,
-    searchProducts,
-    getProductsBySupplier
+  getAllProducts,
+  getSubcategories,
+  getProductByCategory,
+  getProductByShopId,
+  searchProducts,
+  getProductsBySupplier
 }
